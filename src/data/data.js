c1e59714049ef62478699665dfb42acd5120237b
Lila Amelia <lilaamelia77@gmail.com>
2024-07-01 23:24:37 +0700

    finished intro and added projects

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+        title: 'Sneaker Spot',
+        info: 'A modern, responsive e-commerce platform showcasing the latest and most popular sneakers.',
+        techStack: ['React', 'Bootstrap', 'Vite', 'JavaScript', 'HTML', 'CSS'],
+        demo: 'https://sneakerspot.vercel.app/',
+        sourceCode: 'https://github.com/lilaaml/sneakerspot',
+        appCategory: 'big'
+    },
+    {
+        title: 'Arcadia Ticketing',
+        info: 'An online platform that provides users the information regarding movie showtimes and their respective cinemas.',
+        techStack: ['jQuery', 'Bootstrap', 'JavaScript', 'HTML', 'CSS'],
+        demo: 'https://lilaaml.github.io/TicketingApp/',
+        sourceCode: 'https://github.com/lilaaml/TicketingApp',
+        appCategory: 'big'
+    },
+    // {
+    //     title: 'Sarana Door',
+    //     info: 'A full-stack project showcasing a responsive and dynamic online store for PVC-based doors.',
+    //     techStack: ['React', 'Python', 'Django'],
+    //     demo: 'Unavailable',
+    //     sourceCode: 'https://github.com/lilaaml/sd-new',
+    //     appCategory: 'big'
+    // },
+    {
+        title: 'Food Domain',
+        info: 'A mobile app that lists campus cafeterias, allowing users to search by category, name, location, hours, or contact, and bookmark favorites.',
+        techStack: ['Java', 'Dart'],
+        demo: 'Unavailable',
+        sourceCode: 'https://github.com/lilaaml/final-project',
+        appCategory: 'big'
+    },
+    {
+        title: 'To-do List',
+        info: 'A simple to-do list web app that allows users to add new tasks, mark them as finished, and remove them.',
+        techStack: ['JavaScript', 'HTML', 'CSS'],
+        demo: 'https://lilaaml.github.io/todolist-app/',
+        sourceCode: 'https://github.com/lilaaml/todolist-app',
+        appCategory: 'small'
+    },
+    {
+        title: 'Calculator',
+        info: 'A web app that helps users perform various mathematical calculations.',
+        techStack: ['JavaScript', 'HTML', 'CSS'],
+        demo: 'https://lilaaml.github.io/calculator-app/',
+        sourceCode: 'https://github.com/lilaaml/calculator-app',
+        appCategory: 'small'
+    },
+]
