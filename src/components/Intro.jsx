63d60c44fc22b8b9ae16dad0b8bf7312993aa938
Lila Amelia <lilaamelia77@gmail.com>
2024-07-02 23:14:19 +0700

    view is much better now

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,27 +1,17 @@
-import { projects } from '../data/data'
+// import { projects } from '../data/data'
+import Projects from './Projects'
 import './Intro.css'
 
 const Intro = () => {
-  const projectList = projects.map((project) => (
-    // <div key={project.id} className="container-sm flex-wrap justify-content-center my-3"></div>
-    <div key={project.id} id="projectDiv" className="container-sm d-flex flex-column flex-wrap justify-content-center my-3 py-3">
-      <h3 className="fw-bold">{project.title}</h3>
-      <span className="mt-2 lead">{project.info}</span>
-      <span id="techSpan" className="mt-2 lead">{project.techStack}</span>
-      <div id="linkDiv" className="d-flex mt-5">
-        <a href={project.demo} className="btn btn-secondary">Live Demo</a>
-        <a href={project.sourceCode} className="btn btn-secondary">Source Code</a>
-      </div>
-    </div>
-  ))
     return (
       <div className="container-sm d-flex flex-column flex-wrap justify-content-sm-center my-3">
         <h3 className="fw-bold">Hello! I&apos;m Lila, and I have a huge passion for designing, particularly in UI/UX. </h3>
         <p className="lead">With years of experience in frontend development, I&apos;ve honed my skills to create visually appealing, user-friendly, and responsive websites. I&apos;ve developed several projects on my own, showcasing my expertise and creativity. Explore my portfolio to see the projects I&apos;ve brought to life and get a glimpse of my design and development journey.</p>
         <a href="https://drive.google.com/file/d/1BMcv97j8cXZx4kntfjLF6UvIyy6CzO6O/view" className="btn btn-secondary">Download my Portfolio</a>
-        <div className="flex flex-wrap">
+        {/* <div className="flex flex-wrap">
           {projectList}
-        </div>
+        </div> */}
+        <Projects />
       </div>
     )
   }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,30 @@
+import { projects } from '../data/data'
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import './Projects.css'
+
+const Projects = () => {
+    const techArray = (techStack) => {
+        return techStack.map(tech => <li key={tech.id} id="tech-list">{tech}</li>)
+    }
+    const projectList = projects.map((project) => (
+        <div key={project.id} id="project-item" className="pb-3 text-center">
+          <h3 id="project-title" className="fw-bold">{project.title}</h3>
+          <span id="project-info" className="mt-2 lead">{project.info}</span>
+          <div id="tech-grid" className="mt-3 d-flex flex-wrap justify-content-center">{techArray(project.techStack)}</div>
+          <div id="link-grid" className="mt-3 d-flex flex-wrap justify-content-center">
+            <a href={project.demo} className="btn btn-secondary">Live Demo <BsBoxArrowUpRight className="link-icon" /></a>
+            <a href={project.sourceCode} className="btn btn-secondary">Source Code <FaGithub className="link-icon" /></a>
+          </div>
+        </div>
+      ))
+  return (
+    <div className="container-sm justify-content-sm-center">
+      <div id="project-grid" className="d-flex flex-wrap justify-content-center">
+        {projectList}
+      </div>
+    </div>
+  )
+}
+
+export default Projects
