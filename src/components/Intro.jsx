80a1aa4930a4d85370ffad27b99baa66e67d4e1a
Lila Amelia <lilaamelia77@gmail.com>
2024-07-04 22:44:04 +0700

    intro just a little bit more polishing

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,13 +4,11 @@ import './Intro.css'
 const Intro = () => {
     return (
       <div className="container-sm d-flex flex-column flex-wrap justify-content-sm-center mt-5 col-md-6">
-        <h3 className="display-6">Hello! I&apos;m Lila</h3>
+        <h3 className="display-6 fw-bold">Hello! I&apos;m <span className="highlight">Lila</span></h3>
         <p className="lead">I&apos;m a freshgraduate with a huge passion for designing, UI & UX alike. I&apos;ve developed several projects with visually appealing, user-friendly, and responsive websites on my own. Explore my portfolio to see the projects I&apos;ve brought to life and get a glimpse of my design and development journey.</p>
         <div className="download-btn d-flex flex-wrap justify-content-center">
-          <form action="" method="get">
-            <button type="submit" className="btn btn-dark mx-2">Download CV</button>
-          </form>
-          <a href="https://drive.google.com/file/d/1BMcv97j8cXZx4kntfjLF6UvIyy6CzO6O/view" className="btn btn-dark mx-2">Download Portfolio</a>
+          <a href="https://drive.google.com/file/d/1BMcv97j8cXZx4kntfjLF6UvIyy6CzO6O/view" className="cv-btn btn mx-2">Download CV</a>
+          <a href="https://drive.google.com/file/d/1BMcv97j8cXZx4kntfjLF6UvIyy6CzO6O/view" className="portfolio-btn btn mx-2">Download Portfolio</a>
         </div>
       </div>
       // <div className="container-sm my-5 justify-content-sm-center">
