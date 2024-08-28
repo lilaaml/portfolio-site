accd9aedf7817c1b46581aecf968fbb213d42e38
Lila Amelia <lilaamelia77@gmail.com>
2024-08-28 12:53:56 +0700

    intro changed

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
       <div id="intro" className="container-sm d-flex flex-column flex-wrap justify-content-sm-center mt-5 col-md-6">
         <h3 className="display-6 fw-bold">Hey there, I&apos;m <span className="highlight">Lila</span></h3>
 
-        <p className="lead">I&apos;m an aspiring frontend developer. I pour my creativity into creating responsive and visually appealing UI/UX designs for web applications. Explore my portfolio to see the projects I&apos;ve worked on!</p>
+        <p className="lead">I put all of my creativity into making UI/UX designs for web applications that are both aesthetically pleasing and responsive. Explore my portfolio to see the projects I&apos;ve worked on!</p>
 
         <div className="download-btn d-flex flex-wrap justify-content-center">
           <a href={cv} download="CV" className="cv-btn btn mx-2 my-2">Download CV</a>
