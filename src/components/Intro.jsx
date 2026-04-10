d3543dbda27ff7136d529cec92553933674fc951
Lila Amelia <lilaamelia77@gmail.com>
2026-04-10 17:23:31 +0700

    update cv and intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,19 +3,19 @@ import portfolio from '../assets/Portfolio.pdf'
 import './Intro.css'
 
 const Intro = () => {
-    return (
-      <div id="intro" className="container-sm d-flex flex-column flex-wrap justify-content-sm-center mt-5 col-md-6">
-        <h3 className="display-6 fw-bold">Hey there, I&apos;m <span className="highlight">Lila</span></h3>
+  return (
+    <div id="intro" className="container-sm d-flex flex-column flex-wrap justify-content-sm-center mt-5 col-md-6">
+      <h3 className="display-6 fw-bold">Hey there, I&apos;m <span className="highlight">Lila</span></h3>
 
-        <p className="lead">I put all of my creativity into making UI/UX designs for web applications that are both aesthetically pleasing and responsive. Explore my portfolio to see the projects I&apos;ve worked on!</p>
+      <p className="lead">Technical professional bridging software engineering and risk management. I manage the full lifecycle of risk; from code-level vulnerabilities to business process integrity and regulatory compliance. Expert in
+        leveraging this hybrid background for Application Security and Security Consulting.</p>
 
-        <div className="download-btn d-flex flex-wrap justify-content-center">
-          <a href={cv} download="CV" className="cv-btn btn mx-2 my-2">Download CV</a>
-          <a href={portfolio} download="Portfolio" className="portfolio-btn btn btn-secondary mx-2 my-2">Download Portfolio</a>
-        </div>
+      <div className="download-btn d-flex flex-wrap justify-content-center">
+        <a href={cv} download="CV" className="cv-btn btn mx-2 my-2">Download CV</a>
+        <a href={portfolio} download="Portfolio" className="portfolio-btn btn btn-secondary mx-2 my-2">Download Portfolio</a>
       </div>
-    )
-  }
-  
-  export default Intro
-  
+    </div>
+  )
+}
+
+export default Intro
